c2318af986b3dbb71956c515b3348b850218d960
Ana <ana.vega@udea.edu.co>
2025-05-17 22:58:23 -0500

    Despliegue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import React from 'react';
+import { LoginForm } from '@/components/login-form';
+
 export default function Home() {
   return (
-  <>efwvgegvwergv</>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <LoginForm />
+    </div>
   );
 }
